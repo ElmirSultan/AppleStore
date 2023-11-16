c7b215ac88e54b26706267b5b416a74e962f75e1
ElmirSultan <elmirsultann@gmail.com>
2023-11-16 19:10:36 +0400

    mongo db is updated

--- server/index.js
+++ server/index.js
@@ -5,8 +5,8 @@ import mongoose from "mongoose";
 import cors from "cors";
 import dotenv from "dotenv";
 import bodyParser from "body-parser";
-import productRoutes from "./routes/productRoutes.js"
-import authRoutes from "./routes/authRoutes.js"
+import productRoutes from "./routes/productRoutes.js";
+import authRoutes from "./routes/authRoutes.js";
 dotenv.config();
 
 const myapp = express();
@@ -18,15 +18,19 @@ myapp.use(bodyParser.json({ limit: "30mb", extended: true }));
 myapp.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
 myapp.use(cors());
 
+myapp.get("/", (req, res) => res.send("Welcome to Elmir Sultan's project"));
 
-myapp.get("/",(req,res) => res.send("Welcome to Elmir Sultan's project"))
-
-myapp.use("/products",productRoutes)
-myapp.use("/auth",authRoutes);
+myapp.use("/products", productRoutes);
+myapp.use("/auth", authRoutes);
 
 // SETUP
 const PORT = process.env.PORT || 4500;
-mongoose.connect(process.env.MONGO_URL)
-.then(()=> {
-    myapp.listen(PORT, () => console.log(`Server running on port: ${PORT}`))
-}).catch((error) => console.log(`${error}  did not connect`))
+mongoose
+  .connect(process.env.MONGO_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    myapp.listen(PORT, () => console.log(`Server running on port: ${PORT}`));
+  })
+  .catch((error) => console.log(`${error}  did not connect`));
